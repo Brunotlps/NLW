b04202759ba7302003cae4ec6521ccc3a6193aa0
Bruno Teixeira <brunoteixlps@gmail.com>
2024-02-08 21:22:59 -0300

    feat: Implementing Error Handler

---
+++ src/errors/error_handler.py
@@ -0,0 +1,13 @@
+from src.views.http_types.http_response import HttpResponse
+
+
+def handle_errors(error: Exception) -> HttpResponse:
+    return HttpResponse(
+        status_code=500,
+        body={
+            "errors": [{
+                "title": "Server Error",
+                "detail": str(error)
+            }]
+        }
+    )
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,14 +1,16 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 
 class TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest):
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
         # logica de regra de negócio
-        print("Hello")
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
