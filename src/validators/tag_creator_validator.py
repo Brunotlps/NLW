4351d5c73276f5a4dae548d3ad34879487ffcf54
Bruno Teixeira <brunoteixlps@gmail.com>
2024-02-11 08:46:19 -0300

    feat - Implementing Validator

--- src/validators/tag_creator_validator.py
+++ src/validators/tag_creator_validator.py
@@ -1,4 +1,5 @@
 from cerberus import Validator
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 
 def tag_creator_validator(request: any) -> None:
@@ -10,4 +11,4 @@ def tag_creator_validator(request: any) -> None:
 
     response = body_validator.validate(request.json)
     if response is False:
-        raise Exception(body_validator.errors)
+        raise HttpUnprocessableEntityError(body_validator.errors)
